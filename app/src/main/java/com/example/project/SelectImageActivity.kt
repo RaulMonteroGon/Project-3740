c3efac1562247297f002e0969a9d4fc2637a7965
Raúl Montero <rmontegon@gmail.com>
2021-09-29 11:03:40 +0200

    Get img from gallery working

--- app/src/main/java/com/example/project/SelectImageActivity.kt
+++ app/src/main/java/com/example/project/SelectImageActivity.kt
@@ -4,19 +4,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
+import android.widget.ImageView
+import com.google.mlkit.vision.objects.ObjectDetection
+import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions
 
 class SelectImageActivity : AppCompatActivity() {
+    val options = ObjectDetectorOptions.Builder()
+        .setDetectorMode(ObjectDetectorOptions.SINGLE_IMAGE_MODE)
+        .enableMultipleObjects()
+        .enableClassification()  // Optional
+        .build()
+    val objectDetector = ObjectDetection.getClient(options)
+
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+    private val pickImage = 100
+    private var imageUri: Uri? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_image)
 
         val btnsettingsimg : Button = findViewById(R.id.btn_settingsimg)
-
+        imageView = findViewById(R.id.imageView)
 
         btnsettingsimg.setOnClickListener {
-            val settings = Intent(this, SettingsActivity::class.java)
-            startActivity(settings)
+            /*val settings = Intent(this, SettingsActivity::class.java)
+            startActivity(settings)*/
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, pickImage)
+
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            imageView.setImageURI(imageUri)
         }
     }
 }
