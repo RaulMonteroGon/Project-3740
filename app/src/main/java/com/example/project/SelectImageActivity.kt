bbd7ba4b4e5dd1407d3f04f93fc615becbac21bb
Raúl Montero <rmontegon@gmail.com>
2021-10-04 15:07:08 +0200

    Img with text working but not really lol

--- app/src/main/java/com/example/project/SelectImageActivity.kt
+++ app/src/main/java/com/example/project/SelectImageActivity.kt
@@ -1,5 +1,6 @@
 package com.example.project
 
+import android.R.attr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,7 +13,11 @@ import com.google.mlkit.vision.objects.ObjectDetection
 import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions
 import java.io.IOException
 import android.graphics.Bitmap
+import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
+import android.R.attr.data
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler
 
 
 class SelectImageActivity : AppCompatActivity() {
@@ -24,6 +29,7 @@ class SelectImageActivity : AppCompatActivity() {
     val objectDetector = ObjectDetection.getClient(options)
 
     lateinit var imageView: ImageView
+    lateinit var resultTv : TextView
     lateinit var button: Button
     private val pickImage = 100
     private var imageUri: Uri? = null
@@ -35,6 +41,7 @@ class SelectImageActivity : AppCompatActivity() {
 
         val btnsettingsimg : Button = findViewById(R.id.btn_settingsimg)
         imageView = findViewById(R.id.imageView)
+        resultTv = findViewById(R.id.textView)
 
         btnsettingsimg.setOnClickListener {
             /*val settings = Intent(this, SettingsActivity::class.java)
@@ -42,28 +49,34 @@ class SelectImageActivity : AppCompatActivity() {
             val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
             startActivityForResult(gallery, pickImage)
 
+
         }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK && requestCode == pickImage) {
             imageUri = data?.data
+            imageView.setImageURI(imageUri)
             val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri)
             val image = InputImage.fromBitmap(bitmap, 0)
+
+
             objectDetector.process(image)
                 .addOnSuccessListener { detectedObjects ->
                     for (detectedObject in detectedObjects) {
                         val boundingBox = detectedObject.boundingBox
                         val trackingId = detectedObject.trackingId
+                        val text = detectedObject.labels.firstOrNull()?.text?:"Undefined"
+                        /*val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_LONG)
+                        toast.show()*/
+                        resultTv.append(text + "      "+"\n" )
 
-                        //val toast = Toast.makeText(applicationContext, detectedObject.labels[0].text, Toast.LENGTH_LONG)
-                        //toast.show()
                     }
                 }
                 .addOnFailureListener { e ->
 
                 }
-            imageView.setImageURI(imageUri)
+
         }
     }
 }
