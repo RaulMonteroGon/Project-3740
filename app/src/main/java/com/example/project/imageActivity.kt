cece5256d4fb38df181d42e1cf7cfd4d8f3a1a48
Raúl Montero <rmontegon@gmail.com>
2021-10-27 13:55:30 +0200

    App speaks

--- app/src/main/java/com/example/project/imageActivity.kt
+++ app/src/main/java/com/example/project/imageActivity.kt
@@ -7,23 +7,29 @@ import android.widget.TextView
 import android.graphics.BitmapFactory
 
 import android.graphics.Bitmap
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.widget.Toast
+import java.util.*
 
 
 
-
-class imageActivity : AppCompatActivity() {
+class imageActivity : AppCompatActivity(){
 
     lateinit var ivPicture : ImageView
     lateinit var tvResult : TextView
+    private var tts : TextToSpeech? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image)
 
 
-
         ivPicture = findViewById(R.id.ivPhoto)
         tvResult = findViewById(R.id.tvResulttt)
+
+
 
         if (intent.hasExtra("byteArray")) {
             val bitmap = BitmapFactory.decodeByteArray(
@@ -35,6 +41,21 @@ class imageActivity : AppCompatActivity() {
         }
         if(intent.hasExtra("result")){
             tvResult.text = intent.getStringExtra("result")
+            //speak()
             }
+        speak()
     }
+
+
+    private fun speak() {
+        tts = TextToSpeech(applicationContext,TextToSpeech.OnInitListener {
+            if(it==TextToSpeech.SUCCESS){
+                tts!!.language = Locale.US
+                tts!!.setSpeechRate((0.5f))
+                tts!!.speak(intent.getStringExtra("result"),TextToSpeech.QUEUE_FLUSH,null)
+
+            }
+        })
+    }
+
 }
